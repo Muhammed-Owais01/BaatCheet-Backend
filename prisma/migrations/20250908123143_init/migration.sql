4ceee2556fd1e1bf427b1c80acc3015e2316621f
fasihh <fhk.tcs@gmail.com>
2025-09-08 21:30:44 +0500

    feat: add prisma
    - initialize
    - migrations
    - schema
    - service

---
+++ prisma/migrations/20250908123143_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."messages" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
