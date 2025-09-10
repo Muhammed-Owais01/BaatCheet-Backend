578366d0d627cb8f7326f219cfa73eb4cc462740
fasihh <fhk.tcs@gmail.com>
2025-09-11 00:23:29 +0500

    refactor: rename id in messages to messageId

--- packages/db/src/prisma/migrations/20250908123143_init/migration.sql
+++ packages/db/src/prisma/migrations/20250908123143_init/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
 CREATE TABLE "public"."messages" (
-    "id" TEXT NOT NULL,
+    "messageId" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("messageId")
 );
