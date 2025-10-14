0d8f59d298474ed936f7f6c00c05e32ebe07f50f
fasihh <fhk.tcs@gmail.com>
2025-10-14 23:27:55 +0500

    chore: cleanup migrations

--- packages/db/src/prisma/migrations/20250908123143_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "public"."messages" (
-    "messageId" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "messages_pkey" PRIMARY KEY ("messageId")
-);
--- packages/db/src/prisma/migrations/20250909182207_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "messages" DROP CONSTRAINT "messages_senderId_fkey";
-
--- AddForeignKey
-ALTER TABLE "messages" ADD CONSTRAINT "messages_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "users"("userId") ON DELETE CASCADE ON UPDATE CASCADE;
