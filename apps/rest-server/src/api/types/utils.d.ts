19f52f0108f583f57964b22ea7e26bae98ae76cc
fasihh <fhk.tcs@gmail.com>
2025-10-14 23:29:44 +0500

    feat: add transaction client util

--- apps/rest-server/src/api/types/utils.d.ts
+++ apps/rest-server/src/api/types/utils.d.ts
@@ -1,1 +1,3 @@
 export type Creation<T, K extends keyof T> = Omit<T, "createdAt" | "updatedAt" | K>;
+export type TransactionClient = Omit<PrismaClient, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends">;
+
