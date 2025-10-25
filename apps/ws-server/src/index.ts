3de46b8a26b193b2bbaa9f203a4a8941a851df25
Muhammed-Owais01 <workandrecovery@gmail.com>
2025-10-25 16:45:24 +0500

    feat: msg permission check in ws-server

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,7 +1,10 @@
 import http from "http";
 import express from "express";
+import cors from 'cors';
+import morgan from 'morgan';
 import SocketService from "./services/socket.js";
 import { ensureTopics, startDLQConsumer, startMessageConsumer } from "./services/kafka.js";
+import { fgaClient } from "@baatcheet/auth";
 
 async function init() {
   // Initialize OpenFGA authorization model
@@ -12,15 +15,27 @@ async function init() {
 
   const app = express();
   app.use(express.json());
+  app.use(morgan('dev'));
+  app.use(cors())
 
   const socketService = new SocketService();
   const httpServer = http.createServer(app);
   const PORT = process.env.PORT ? process.env.PORT : 8000;
 
   // HTTP endpoint for sending messages
-  app.post('/api/messages', async (req, res) => {
+  app.post('/api/messages/:guildId', async (req, res) => {
     try {
+      const { guildId } = req.params;
       const { chatId, senderId, message } = req.body;
+
+      const canSendMessage = await fgaClient.check({
+        user: `user:${senderId}`,
+        relation: 'can_send_messages',
+        object: `guild:${guildId}`,
+      });
+      if (!canSendMessage) {
+        return res.status(403).json({ success: false, message: 'User does not have permission to send messages in this guild' });
+      }
       
       // Use the same Redis publisher as socket service
       await socketService.publishMessage(chatId, senderId, message);
