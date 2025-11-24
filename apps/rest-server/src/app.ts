050473dfb348072a1e5e3432520a1f469ac6eb70
fasihh <fhk.tcs@gmail.com>
2025-11-25 00:32:08 +0500

    feat: bind backend to origin

--- apps/rest-server/src/app.ts
+++ apps/rest-server/src/app.ts
@@ -15,7 +15,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || '*',
+}));
 
 app.use('/users', userRouter);
 app.use('/chats', chatRouter);
