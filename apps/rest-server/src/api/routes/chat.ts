0e55bac802f67bda4e2a18f4e1c8a893abdfb808
fasihh <fhk.tcs@gmail.com>
2025-11-25 00:32:47 +0500

    feat: extend chat functionality

--- apps/rest-server/src/api/routes/chat.ts
+++ apps/rest-server/src/api/routes/chat.ts
@@ -6,6 +6,8 @@ import authHandler from '../middlewares/auth-handler.js';
 const router: Router = Router();
 
 router.get('/dms', authHandler, asyncHandler(ChatController.getAllDMChatsByUserId));
-router.get('/dms/:chatId/messages', authHandler, asyncHandler(ChatController.getAllMessagesByChatId));
+router.get('/:chatId', authHandler, asyncHandler(ChatController.getChatById));
+router.get('/guild/:guildId', authHandler, asyncHandler(ChatController.getAllGuildChatsByGuildId));
+router.get('/:chatId/messages', authHandler, asyncHandler(ChatController.getAllMessagesByChatId));
 
 export default router;
